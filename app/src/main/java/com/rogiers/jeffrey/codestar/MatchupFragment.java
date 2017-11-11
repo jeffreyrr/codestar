2e9968265af25493233a8f4b14c701c3e3c41d83
Jeffrey Rogiers <jeffrey.rogiers@gmail.com>
2017-11-10 19:29:06 -0800

    Input validation and clearer wording

--- app/src/main/java/com/rogiers/jeffrey/codestar/MatchupFragment.java
+++ app/src/main/java/com/rogiers/jeffrey/codestar/MatchupFragment.java
@@ -1,12 +1,17 @@
 package com.rogiers.jeffrey.codestar;
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,12 +20,53 @@ public class MatchupFragment extends Fragment {
 
     private static final String TAG = "MatchupFragment";
 
+    private EditText mUser1;
+    private EditText mUser2;
+    private Button mCompareUsers;
+
     public static Fragment newInstance() {
         return new MatchupFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_matchup, container, false);
+        View v = inflater.inflate(R.layout.fragment_matchup, container, false);
+
+        mUser1 = v.findViewById(R.id.text_user_1);
+        mUser2 = v.findViewById(R.id.text_user_2);
+
+        mCompareUsers = v.findViewById(R.id.button_compare_users);
+        mCompareUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!validateUsername(mUser1) || !validateUsername(mUser2)){
+                    return;
+                }
+
+                ArrayList<String> users = new ArrayList<>();
+                users.add(mUser1.getText().toString());
+                users.add(mUser2.getText().toString());
+            }
+        });
+        return v;
+    }
+
+    private boolean validateUsername(EditText editText) {
+        // Username requirements according to Github:
+        // Username may only contain alphanumeric characters or single hyphens,
+        // and cannot begin or end with a hyphen.
+        // This is a simplified version of the requirements that only focuses on
+        // the character set in order to provide useful feedback.
+        String SIMPLE_USERNAME_PATTERN = "^[A-Za-z0-9-]+$";
+
+        Pattern pattern = Pattern.compile(SIMPLE_USERNAME_PATTERN);
+        Matcher matcher = pattern.matcher(editText.getText().toString());
+
+        if(!matcher.matches()){
+            editText.setError(getString(R.string.invalid_username));
+            return false;
+        }
+
+        return true;
     }
 }
